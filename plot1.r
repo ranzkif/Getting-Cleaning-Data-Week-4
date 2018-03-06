557ec90e8f1b6117c3c6b421081c9d24e2e53e84
Rolando R. Fajardo <32696216+ranzkif@users.noreply.github.com>
2018-03-06 12:19:28 +0800

    Project 1 resubmit

---
+++ plot1.r
@@ -0,0 +1,7 @@
+#reading of file from my local drive
+powerfile <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(powerfile) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(powerfile,powerfile$Date=="1/2/2007" | powerfile$Date =="2/2/2007")
+
+#plotting of data
+hist(as.numeric(as.character(subpower$Global_active_power)), col ="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)",ylab ="Frequency")
